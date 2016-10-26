1d14aaa603317cb51c9659afdf2089a469965257
Matt Dennis <msd7734@rit.edu>
2016-10-26 16:18:51 -0400

    Pull test code into sep. methods

--- Csci351DC1ftp/Program.cs
+++ Csci351DC1ftp/Program.cs
@@ -8,27 +8,48 @@ namespace Csci351DC1ftp
 {
     class Program
     {
-        public static void Main(string[] args)
+        static void SnipBitsTest()
         {
-            /*
             // 0b10010111
             byte b = 0x97;
             Console.WriteLine(Bits.SnipBits(b, new byte[]{0, 1, 2}));
             Console.WriteLine(Bits.SnipBits(b, new byte[] { 7 }));
-            */
+        }
 
+        static void NackPacketTest()
+        {
             NackPacket p = new NackPacket(1);
             byte[] bytes = p.GetBytes();
+            PrintByteArr(bytes);
 
+            Console.WriteLine(BitConverter.ToInt16(bytes, 0));
+            Console.WriteLine(BitConverter.ToInt16(bytes, 2));
+        }
+
+        static void ErrPacketTest()
+        {
+            ErrorPacket p = new ErrorPacket(4, "Some crap went wrong.");
+            byte[] bytes = p.GetBytes();
+            PrintByteArr(bytes);
+
+            Console.WriteLine(BitConverter.ToInt16(bytes, 0));
+            Console.WriteLine(BitConverter.ToInt16(bytes, 2));
+            Console.WriteLine(Encoding.ASCII.GetString(bytes, 4, bytes.Length - 4));
+        }
+
+        static void PrintByteArr(byte[] bytes)
+        {
             foreach (byte b in bytes)
             {
                 Console.Write("{0} ", Bits.ByteToBinStr(b));
             }
-
             Console.WriteLine();
-            Console.WriteLine(BitConverter.ToInt16(bytes, 0));
-            Console.WriteLine(BitConverter.ToInt16(bytes, 2));
-             
+        }
+
+        public static void Main(string[] args)
+        {
+            ErrPacketTest();
+
             // wait to end
             Console.ReadKey(true);
         }

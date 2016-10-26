fcf538149937ac8a8e36f6c3b97c171c0e0adbd0
Matt Dennis <msd7734@rit.edu>
2016-10-26 16:20:54 -0400

    Implement ErrorPacket

--- Csci351DC1ftp/ErrorPacket.cs
+++ Csci351DC1ftp/ErrorPacket.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace Csci351DC1ftp
 {
-    public class ErrorPacket
+    public class ErrorPacket : TFTPPacket
     {
+        public short ErrorNum { get; private set; }
+
+        public string ErrorMsg { get; private set; }
+
+        public ErrorPacket(short errNum, string errMsg) : base(Opcode.ERROR)
+        {
+            ErrorNum = errNum;
+            ErrorMsg = errMsg;
+        }
+
+        public override byte[] GetBytes()
+        {
+            // remember converting str => bytes does NOT include null terminator
+
+            byte[] opcBytes = BitConverter.GetBytes((short)this.Opcode);
+            byte[] errnBytes = BitConverter.GetBytes(ErrorNum);
+            byte[] errmsgBytes = Encoding.ASCII.GetBytes(ErrorMsg);
+            byte[] res = { 0x0 };
+
+            return opcBytes.Concat(errnBytes)
+                .Concat(errmsgBytes)
+                .Concat(res).ToArray();
+        }
     }
 }

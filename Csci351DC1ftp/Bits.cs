29a4e87aa3039b906eef000173c3a6e5497d3397
Matt Dennis <msd7734@rit.edu>
2016-10-25 21:45:24 -0400

    Add project

---
+++ Csci351DC1ftp/Bits.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csci351DC1ftp
+{
+    // Hold useful methods for bit manipulation
+    public static class Bits
+    {
+        public static byte NthBit(byte b, byte nbit)
+        {
+            if (nbit > 7 || nbit < 0)
+                throw new BitOpException("The bit index provided was out of bounds for a byte.");
+
+            return Convert.ToByte(((b >> nbit) & 0x01));
+        }
+    }
+
+    public class BitOpException : Exception
+    {
+        private static string defaultMessage = "An invalid bit operation was performed.";
+
+        public BitOpException() : base(defaultMessage) { }
+
+        public BitOpException(string message) : base(message) { }
+    }
+}
---
+++ Csci351DC1ftp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csci351DC1ftp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            byte b = 0x97;
+
+            
+            // wait to end
+            Console.ReadKey(true);
+        }
+    }
+}
